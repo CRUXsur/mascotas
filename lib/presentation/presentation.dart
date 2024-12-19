f38e2c68c44ae37317b1ae83055d0a1482e0a3c2
Marco Torrico <marcojtorrico@gmail.com>
2024-12-19 15:54:39 -0400

    permissions provider

--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,5 +1,10 @@
-
+//lib
+// presentation
 
 
 //providers
 export 'package:mascotas/presentation/providers/app_state_provider.dart';
+
+
+// permissions
+  export 'package:mascotas/presentation/providers/permissions/permissions_provider.dart';
